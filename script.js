ec705db0e7d7b4a6f752eeaaa490feb147042862
samruddhit28 <samruddhi.thopate23@stmirascollegepune.edu.in>
2024-09-04 16:47:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Smooth scrolling for navigation links
+//Smooth scrolling for navigation links
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
